71336e315ca253f26602a3c76bde844b0add2f38
Alexander <o.y.dyskyi@student.khai.edu>
2023-10-21 13:01:45 +0300

    ChatBot 1st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,14 @@
+package ChatBot;
+
+public class ChatBot {
+    public static void main(String[] args) {
+
+        String botName = "Hyron Stick";
+        int currentYear = java.time.Year.now().getValue();
+
+
+        System.out.println("Hello! My name is " + botName + ".");
+        System.out.println("I was created in " + currentYear + ".");
+
+    }
+}
