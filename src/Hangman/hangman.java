ac3b09aab3e8f974904f689e382c70e4dbc07135
Alexander <o.y.dyskyi@student.khai.edu>
2023-11-02 00:07:29 +0200

    Комміт 1: Додано Ð

---
+++ src/Hangman/hangman.java
@@ -0,0 +1,9 @@
+package Hangman;
+
+public class hangman { public static void main(String[] args) {
+    System.out.println("HANGMAN");
+    System.out.println("Вітаємо у грі 'Шибениця'!");
+    System.out.println("Гра буде доступна незабаром.");
+}
+
+}
