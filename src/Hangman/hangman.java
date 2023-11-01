8b034c88b57ac0f16a53fa729b9cb5d31ca995c5
Alexander <o.y.dyskyi@student.khai.edu>
2023-11-02 00:18:51 +0200

    Комміт 1: Д 2

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,9 +1,23 @@
 package Hangman;
 
-public class hangman { public static void main(String[] args) {
-    System.out.println("HANGMAN");
-    System.out.println("Вітаємо у грі 'Шибениця'!");
-    System.out.println("Гра буде доступна незабаром.");
-}
+import java.util.Scanner;
 
+public class hangman {
+    public static void main(String[] args) {
+        System.out.println("HANGMAN");
+        System.out.println("Вітаємо у грі 'Шибениця'!");
+        System.out.println("Гра буде доступна незабаром.");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Guess the word: > ");
+        String guessedWord = scanner.nextLine().trim();
+
+        String targetWord = "java";
+
+        if (guessedWord.equalsIgnoreCase(targetWord)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
+    }
 }
